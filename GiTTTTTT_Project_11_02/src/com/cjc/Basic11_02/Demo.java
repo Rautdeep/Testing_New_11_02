e5653f132e33900955af12ddf2b757b210acf14a
Admin <Admin@Admin-PC>
2022-02-11 11:10:03 +0530

    By Deepak

--- GiTTTTTT_Project_11_02/src/com/cjc/Basic11_02/Demo.java
+++ GiTTTTTT_Project_11_02/src/com/cjc/Basic11_02/Demo.java
@@ -2,9 +2,14 @@ package com.cjc.Basic11_02;
 
 public class Demo 
 {
-public static void main(String[] args) 
+	
+	public void m1()
+	{
+		System.out.println("M1");
+	}
+    public static void main(String[] args) 
 
-{
+    {
 	System.out.println("Hello world");
-}
+    }
 }
